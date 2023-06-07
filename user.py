f99ac0b8dc0b7923459765e7be64cf67396b3ce5
Chesnokov M.A <chesnokov_ma@mail.ru>
2023-06-07 19:03:08 +0500

    commit after git restore --source=d35d32ecaa52b0d7dd232975abb577f678e235b0 --staged .

--- user.py
+++ user.py
@@ -1,13 +1,16 @@
 import games
+import logging_game
 
 # анализ координат, введенных пользователем
 def user_input(pole, arg):
     try:
         (user_in_line,user_in_column) = [int(x) for x in arg]
     except:
+        logging_game.logger.exception(f'Ошибка установки координат пользователем: {arg}; pole: {pole}')
         return 'Необходимо ввести целые числа!'
     else:
         if games.check_coordinates(pole, (user_in_line, user_in_column)):
             return (user_in_line, user_in_column)
         else:
+            logging_game.logger.info(f'Указанная ячейка занята: {arg}; pole: {pole}')
             return 'Указанная ячейка занята, выбирете другую: '
